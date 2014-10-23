9a2d98c454c418689c7efe7da37f65caba15390d
Bob McWhirter <bob@mcwhirter.org>
2014-10-23 09:45:58 -0400

    Actually resolve and download maven artifacts into a local repository.

--- spec/artifact-spec.js
+++ spec/artifact-spec.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var maven  = require( './../index' );
+var fs = require('fs');
 
 describe( 'artifacts', function() {
 
@@ -10,5 +11,14 @@ describe( 'artifacts', function() {
     var versions = artifact.versions;
     assert.ok( versions.length > 0 );
   })
+
+  it ('should be able to install a specific version', function() {
+    var session = new maven.Session('./local-maven-repo');
+    var project = session.project( 'log4j', 'log4j' );
+    var artifact = project.artifact( 'jar' );
+    var path = artifact.install();
+    assert.notEqual( path, undefined );
+    assert.ok( fs.existsSync( path ) );
+  });
 
 });
